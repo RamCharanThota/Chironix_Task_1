c588a19788e73a5d461412fcda88067c9f673b3f
Ram Charan Thota <icherryme.0407@gmail.com>
2022-02-03 12:13:42 +1100

    duration logic is added to cpp file

--- my_nodes/src/rotate_husky_clockwise.cpp
+++ my_nodes/src/rotate_husky_clockwise.cpp
@@ -2,6 +2,8 @@
 #include <geometry_msgs/Twist.h> 
 #include <stdlib.h> 
 
+double required_duration=10;
+
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "rotate_husky_clockwise");
@@ -15,12 +17,22 @@ int main(int argc, char **argv)
   {
    geometry_msgs::Twist cmd_msg;
    cmd_msg.angular.z=0.5;
-   pub.publish(cmd_msg);
-   ros::spinOnce();
-
-    loop_rate.sleep();
-  }
-
+   /*pub.publish(cmd_msg);
+     ros::spinOnce();
+     loop_rate.sleep();*/
+    ros::Time beginTime = ros::Time::now();
+    ros::Time endTime =ros::Time::now(); 
+    double duration =  ros::Duration(endTime-beginTime).toSec();
+    while(duration < required_duration )
+    {
+        pub.publish(cmd_msg);
+        ros::Duration(0.1).sleep();
+        endTime =ros::Time::now();
+        duration =  ros::Duration(endTime-beginTime).toSec(); 
+       
+    }
 
   return 0;
+  }
+
 }
